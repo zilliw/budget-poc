a0de642930f239bef324b508e313d5e35489835c
william.azis <azis.william@gmail.com>
2020-09-14 14:58:55 +0200

    update table crud

--- web-ui/src/views/TheMenu.vue
+++ web-ui/src/views/TheMenu.vue
@@ -107,101 +107,7 @@
 
         <Timesheet/>
 
-
     </v-main>
-    <v-btn
-      bottom
-      color="pink"
-      dark
-      fab
-      fixed
-      right
-      @click="dialog = !dialog"
-    >
-      <v-icon>mdi-plus</v-icon>
-    </v-btn>
-    <v-dialog
-      v-model="dialog"
-      width="800px"
-    >
-      <v-card>
-        <v-card-title class="grey darken-2">
-          Create contact
-        </v-card-title>
-        <v-container>
-          <v-row class="mx-2">
-            <v-col
-              class="align-center justify-space-between"
-              cols="12"
-            >
-              <v-row
-                align="center"
-                class="mr-0"
-              >
-                <v-avatar
-                  size="40px"
-                  class="mx-3"
-                >
-                  <img
-                    src="//ssl.gstatic.com/s2/oz/images/sge/grey_silhouette.png"
-                    alt=""
-                  >
-                </v-avatar>
-                <v-text-field
-                  placeholder="Name"
-                ></v-text-field>
-              </v-row>
-            </v-col>
-            <v-col cols="6">
-              <v-text-field
-                prepend-icon="mdi-account-card-details-outline"
-                placeholder="Company"
-              ></v-text-field>
-            </v-col>
-            <v-col cols="6">
-              <v-text-field
-                placeholder="Job title"
-              ></v-text-field>
-            </v-col>
-            <v-col cols="12">
-              <v-text-field
-                prepend-icon="mdi-mail"
-                placeholder="Email"
-              ></v-text-field>
-            </v-col>
-            <v-col cols="12">
-              <v-text-field
-                type="tel"
-                prepend-icon="mdi-phone"
-                placeholder="(000) 000 - 0000"
-              ></v-text-field>
-            </v-col>
-            <v-col cols="12">
-              <v-text-field
-                prepend-icon="mdi-text"
-                placeholder="Notes"
-              ></v-text-field>
-            </v-col>
-          </v-row>
-        </v-container>
-        <v-card-actions>
-          <v-btn
-            text
-            color="primary"
-          >More</v-btn>
-          <v-spacer></v-spacer>
-          <v-btn
-            text
-            color="primary"
-            @click="dialog = false"
-          >Cancel</v-btn>
-          <v-btn
-            text
-            @click="dialog = false"
-          >Save</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
   </v-app>
 </template>
 
@@ -219,8 +125,17 @@
       dialog: false,
       drawer: null,
       items: [
-        { icon: 'mdi-cog', text: 'Paramétrage' },
         { icon: 'mdi-account-clock', text: 'Saisie des temps' },
+        {
+          icon: 'mdi-chevron-up', 'icon-alt' : 'mdi-chevron-down',
+          text: 'Configuration',
+          model: false,
+          children: [
+            { text: 'Liste des société' },
+            { text: 'Liste des consultants' },
+          ],
+        },
+
         {
           icon: 'mdi-chevron-up',
           'icon-alt': 'mdi-chevron-down',
@@ -229,7 +144,7 @@
           children: [
             { text: 'Chiffre d affaire' },
             { text: 'Couts de revient' },
-            { text: 'Print' },
+            { text: 'Statistique' },
           ],
         },
       ],
--- web-ui/src/views/Timesheet.vue
+++ web-ui/src/views/Timesheet.vue
@@ -3,7 +3,7 @@
     :headers="headers"
     :items="desserts"
     sort-by="calories"
-    class="elevation-1"
+    class="elevation-1, table"
   >
     <template v-slot:top>
       <v-toolbar flat color="white">
@@ -87,15 +87,15 @@
       dialog: false,
       headers: [
         {
-          text: 'Dessert (100g serving)',
+          text: 'Mois',
           align: 'start',
           sortable: false,
           value: 'name',
         },
-        { text: 'Calories', value: 'calories' },
-        { text: 'Fat (g)', value: 'fat' },
-        { text: 'Carbs (g)', value: 'carbs' },
-        { text: 'Protein (g)', value: 'protein' },
+        { text: 'Anis', value: 'calories' },
+        { text: 'Nasreddine', value: 'fat' },
+        { text: 'Houssem', value: 'carbs' },
+        { text: 'Ahmed', value: 'carbs' },
         { text: 'Actions', value: 'actions', sortable: false },
       ],
       desserts: [],
@@ -105,14 +105,12 @@
         calories: 0,
         fat: 0,
         carbs: 0,
-        protein: 0,
       },
       defaultItem: {
         name: '',
         calories: 0,
         fat: 0,
         carbs: 0,
-        protein: 0,
       },
     }),
     computed: {
@@ -136,70 +134,24 @@
             calories: 159,
             fat: 6.0,
             carbs: 24,
-            protein: 4.0,
           },
           {
             name: 'Ice cream sandwich',
             calories: 237,
             fat: 9.0,
             carbs: 37,
-            protein: 4.3,
           },
           {
             name: 'Eclair',
             calories: 262,
             fat: 16.0,
             carbs: 23,
-            protein: 6.0,
           },
           {
             name: 'Cupcake',
             calories: 305,
             fat: 3.7,
             carbs: 67,
-            protein: 4.3,
-          },
-          {
-            name: 'Gingerbread',
-            calories: 356,
-            fat: 16.0,
-            carbs: 49,
-            protein: 3.9,
-          },
-          {
-            name: 'Jelly bean',
-            calories: 375,
-            fat: 0.0,
-            carbs: 94,
-            protein: 0.0,
-          },
-          {
-            name: 'Lollipop',
-            calories: 392,
-            fat: 0.2,
-            carbs: 98,
-            protein: 0,
-          },
-          {
-            name: 'Honeycomb',
-            calories: 408,
-            fat: 3.2,
-            carbs: 87,
-            protein: 6.5,
-          },
-          {
-            name: 'Donut',
-            calories: 452,
-            fat: 25.0,
-            carbs: 51,
-            protein: 4.9,
-          },
-          {
-            name: 'KitKat',
-            calories: 518,
-            fat: 26.0,
-            carbs: 65,
-            protein: 7,
           },
         ]
       },
@@ -210,7 +162,7 @@
       },
       deleteItem (item) {
         const index = this.desserts.indexOf(item)
-        confirm('Are you sure you want to delete this item?') && this.desserts.splice(index, 1)
+        confirm('Êtes-vous sur de vouloir supprimer cette ligne?') && this.desserts.splice(index, 1)
       },
       close () {
         this.dialog = false
@@ -230,3 +182,13 @@
     },
   }
 </script>
+
+<style scoped>
+
+.table {
+  margin-left: 10px;
+  margin-right: 10px;
+}
+
+
+</style>
